714080bcb248bb224f9eaaa7bfd002396b95e503
Oscar Ko <OscarKoEmail@gmail.com>
2019-09-10 13:19:08 -0400

    Create New and Create Fic routes, and create pages

--- app.js
+++ app.js
@@ -35,14 +35,34 @@ app.get("/front", function(req, res){
 });
 
 // Fic Routes ===================================
+// INDEX ROUTE
 app.get("/fics", function (req, res){
   Fic.find({}, function(err, foundFics){
     if (err) {
       console.log(err);
+      res.redirect("back");
     } else {
       res.render("fics/index", {fics: foundFics});
     }
   });
+});
+
+// NEW ROUTE
+app.get("/fics/new", function(req, res){
+  res.render("fics/new");
+});
+
+// CREATE ROUTE
+app.post("/fics", function(req, res){
+
+  Fic.create(req.body.fic, function(err, newFic){
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/fics");
+    }
+  })
 });
 
 
