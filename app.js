18416159ea7b81d38682158adc119eaa1e947a41
Oscar Ko <OscarKoEmail@gmail.com>
2019-10-09 14:19:25 -0400

    npm install passport-facebook

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 const Fic = require("./models/fic");
 const User = require("./models/user");
